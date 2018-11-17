8193b4340118407232481f32d482392035b4bf4f
Tommy Parnell <tommy@terribledev.io>
2018-11-17 13:16:16 -0500

    forms

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -5,7 +5,7 @@ const initialValues = {
   message: '',
 };
 
-class ContactForm extends Component {
+class Form extends Component {
   state = initialValues;
 
   handleChange = event => {
@@ -23,50 +23,62 @@ class ContactForm extends Component {
     event.preventDefault();
     this.props.onSubmit(this.state);
   };
+  render() {
+    return this.props.render({
+      values: this.state,
+      handleChange: this.handleChange,
+      handleSubmit: this.handleSubmit
+    });
+  }
+}
 
+class ContactForm extends Component {
+  renderView = ({values, handleChange, handleSubmit }) => 
+   <form onSubmit={handleSubmit}>
+    <div className="field">
+      <div className="control">
+        <input
+          className="input"
+          placeholder="Taylor Swift"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+          data-testid="name-input"
+        />
+        <p className="help is-danger">{null}</p>
+      </div>
+    </div>
+    <div className="field">
+      <div className="control">
+        <textarea
+          className="textarea"
+          placeholder="messsage"
+          type="text"
+          name="message"
+          value={values.message}
+          onChange={handleChange}
+          data-testid="message-input"
+        />
+        <p className="help is-danger">{null}</p>
+      </div>
+    </div>
+    <br />
+    <div className="field">
+      <div className="control">
+        <input
+          type="submit"
+          value="Submit Form"
+          className="button is-primary"
+          data-testid="submit-input"
+        />
+      </div>
+    </div>
+  </form>;
+  
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="field">
-          <div className="control">
-            <input
-              className="input"
-              placeholder="Taylor Swift"
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              data-testid="name-input"
-            />
-            <p className="help is-danger">{null}</p>
-          </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <textarea
-              className="textarea"
-              placeholder="messsage"
-              type="text"
-              name="message"
-              value={this.state.message}
-              onChange={this.handleChange}
-              data-testid="message-input"
-            />
-            <p className="help is-danger">{null}</p>
-          </div>
-        </div>
-        <br />
-        <div className="field">
-          <div className="control">
-            <input
-              type="submit"
-              value="Submit Form"
-              className="button is-primary"
-              data-testid="submit-input"
-            />
-          </div>
-        </div>
-      </form>
+      <Form onSubmit={this.props.onSubmit} render={this.renderView}></Form>
     );
   }
 }
